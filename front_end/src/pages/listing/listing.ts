60abf8eb42aaf558467acba8eeda4062437e7640
Kamran Ali <kamran_ali@hotmail.com>
2017-10-29 22:50:51 +0500

    chart copy

--- front_end/src/pages/listing/listing.ts
+++ front_end/src/pages/listing/listing.ts
@@ -6,6 +6,7 @@ import 'rxjs/Rx';
 
 import { ListingModel } from './listing.model';
 import { ListingService } from './listing.service';
+import { ChartsService } from '../charts/charts.service';
 
 
 @Component({
@@ -15,10 +16,17 @@ import { ListingService } from './listing.service';
 export class ListingPage {
   listing: ListingModel = new ListingModel();
   loading: any;
-
+	type: any = 'line';
+	  data = {labels:[], datasets:[]};
+	  options = {
+		responsive: true,
+		maintainAspectRatio: false,
+		
+	  };
   constructor(
     public nav: NavController,
     public listingService: ListingService,
+    public chartsService: ChartsService,
     public loadingCtrl: LoadingController
   ) {
     this.loading = this.loadingCtrl.create();
@@ -37,7 +45,36 @@ export class ListingPage {
         this.loading.dismiss();
       });
   }
+	ionViewDidEnter() {
 
+		if (window.navigator.onLine) {
+			this.chartsService
+			.getDataFromServer()
+			.then(data => {
+				console.log(data);
+				let items = data.rmGraficoVentasPlan;
+				var yaxis1 = [];
+				var yaxis2 = [];
+				let labels = [];
+				for(var i=0; i< items.length; i++) {
+					/*let y1 = {'x' : items[i].date_order, 'y' : items[i].quantity};
+					let y2 = {'x' : items[i].date_order, 'y' : items[i].plan};
+					yaxis1.push(y1);
+					yaxis2.push(y2);*/
+					labels.push(items[i].date_order);
+					yaxis1.push(items[i].quantity);
+					yaxis2.push(items[i].plan);
+				}
+				let y1 = {'label' : 'Quantity', data: yaxis1,fill: false, borderColor: 'rgb(255, 99, 132)',backgroundColor: 'rgb(255, 99, 132)',};
+				let y2 = {'label' : 'Plan', data: yaxis2,fill: false,  borderColor: 'rgb(54, 162, 235)',backgroundColor: 'rgb(54, 162, 235)',};
+				this.data.datasets.push(y1);
+				this.data.datasets.push(y2);
+				this.data.labels = labels;
+				console.log(this.data);
+				
+			});
+		}
+	}
 
   goToFeed(category: any) {
     console.log("Clicked goToFeed", category);
