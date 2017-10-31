b48b9e43963bfd7b76671f64ffc1e60a711d6ace
Kamran Ali <kamran_ali@hotmail.com>
2017-10-31 14:18:20 +0500

    idb error

--- front_end/src/pages/clientes/clientes.service.ts
+++ front_end/src/pages/clientes/clientes.service.ts
@@ -17,56 +17,7 @@ export class ClientesService {
     public syncService: SyncService
   ) {}
 
-  // Obtener datos de PouchDB
-  getDataFromPouch(): Promise<any> {
-		return this.getAllCustomersFromPouchDB()
-		.then(response => response)
-		.catch(this.handleError);
-	}
-
-  // NO USAR Obtener datos directamente del servidor para el la pagina clientes
-  getData(): Promise<ClientesModel> {
-    console.log("getData");
-    return this.jsonp.request('http://odoo.romilax.com/organica/back_end/rmXMLRPC_clientes.php?task=rmListaClientes&callback=JSONP_CALLBACK',{method:'Get'})
-     .toPromise()
-     .then(
-       response => response.json() as ClientesModel
-
-     )
-     .catch(this.handleError);
-  }
-
-  // Obtener clientes de PouchDB
-  getAllCustomersFromPouchDB() {
-    if (!this.syncService._db)
-      this.syncService.initDB();
-    return new Promise(resolve => {
-      this.syncService._db.allDocs({
-        include_docs: true,
-        startkey: 'cliente',
-        endkey: 'cliente\ufff0'
-      })
-        .then(docs => {
-
-          // console.log('docs1 ->' + JSON.stringify(docs.rows));
-
-          // Each row has a .doc object and we just want to send an
-          // array of customer objects back to the calling controller,
-          // so let's map the array to contain just the .doc objects.
-
-          this._clientes = docs.rows.map(row => {
-            // Dates are not automatically converted from a string.
-            if (row.doc.type == "cliente")
-              // console.log('docs2 ->' + JSON.stringify(row.doc));
-              return row.doc;
-          });
-
-          // Listen for changes on the database.
-          resolve(this._clientes);
-        });
-
-    });
-  }
+  
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
--- front_end/src/pages/sync/sync.service.ts
+++ front_end/src/pages/sync/sync.service.ts
@@ -10,81 +10,16 @@ export class SyncService {
   public _db;
   private _clientes;
 
-  constructor(
-    public http: Http,
-    public jsonp: Jsonp,
-    private platform: Platform,
-    public alertCtrl: AlertController
-  ) {
-  }
+	constructor(
+		public http: Http,
+		public jsonp: Jsonp,
+		private platform: Platform,
+		public alertCtrl: AlertController
+	) {
+		
+	}
 
   
 
-  // obtainCustomersFromServer2PouchDB(): Promise<ClientesModel> {
-  // }
-
-  // getDataFromServer(): Promise<CustomersModel> {
-  getDataFromServer(): Promise<ClientesModel> {
-		return this.jsonp.request('http://odoo.romilax.com/organica/back_end/rmXMLRPC_clientes.php?task=rmListaClientes&callback=JSONP_CALLBACK',{method:'Get'})
-		.toPromise()
-    .then(response => response.json() as ClientesModel)
-		// .then(response.json())
-		.catch(this.handleError);
-	}
-
-  getDataFromPouch(): Promise<any> {
-
-		return this.getAllCustomers()
-		.then(response => response)
-		.catch(this.handleError);
-
-	}
-
-  addCustomer(cliente) {
-    let id = "cliente-" + cliente.id;
-    let db = this._db;
-    cliente._id = "cliente-" + cliente.id;
-    cliente.type = "cliente";
-    this._db.get(id).then(function(doc) {
-      return doc;
-    }).catch(function(err) {
-      console.log(err);
-      return db.put(cliente);
-    });
-
-  }
-
-  private handleError(error: any): Promise<any> {
-    console.error('ERROR CARAJITO:', error); // for demo purposes only
-    return Promise.reject(error.message || error);
-  }
-
-  getAllCustomers() {
-    if (!this._db)
-      this.initDB();
-    return new Promise(resolve => {
-      this._db.allDocs({
-        include_docs: true,
-        startkey: 'customer',
-        endkey: 'customer\ufff0'
-      })
-        .then(docs => {
-
-          // Each row has a .doc object and we just want to send an
-          // array of customer objects back to the calling controller,
-          // so let's map the array to contain just the .doc objects.
-
-          this._clientes = docs.rows.map(row => {
-            // Dates are not automatically converted from a string.
-            if (row.doc.type == "customer")
-              return row.doc;
-          });
-
-          // Listen for changes on the database.
-          resolve(this._clientes);
-        });
-
-    });
-  }
 
 }
