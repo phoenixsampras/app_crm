de485fa258c2842b46afbb81c0e13f91924794c9
Carlos Paz <phoenixsampras@gmail.com>
2017-11-12 10:51:32 -0400

    zebra find

--- front_end/src/pages/print-order/print-order.ts
+++ front_end/src/pages/print-order/print-order.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { zbtprinter } from 'ionic-native';
+// import { zbtprinter } from 'ionic-native';
 import { ViewController, NavController, NavParams, ToastController } from 'ionic-angular';
 /**
  * Generated class for the SkillAutocompletePage page.
@@ -7,68 +7,82 @@ import { ViewController, NavController, NavParams, ToastController } from 'ionic
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
- declare var cordova;
+declare var cordova;
 @Component({
   selector: 'print-order-page',
   templateUrl: 'print-order.html',
 })
 export class PrintOrderPage {
 
-	orderObj:any;
-	address = '';
-	constructor(public navCtrl: NavController,
-		public navParams: NavParams,
-		public viewCtrl: ViewController,
-		public toastCtrl: ToastController,
+  orderObj: any;
+  address = '';
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public viewCtrl: ViewController,
+    public toastCtrl: ToastController,
 
-	) {
-		this.orderObj = this.navParams.get('order');
-		cordova.plugins.zbtprinter.find(
-			function(result) {
-				if(typeof result == 'string') {
-					this.address = result;
-				} else {
-					this.address = result.address;
-				}
-				alert('Zbtprinter: connect success: ' + this.address);
-			}, function(error) {
-				alert('Zbtprinter: connect fail: ' + error);
-			}
-		);
-	}
+  ) {
+    this.orderObj = this.navParams.get('order');
+    cordova.plugins.zbtprinter.find(
+      function(result) {
+        if (typeof result == 'string') {
+          this.address = result;
+        } else {
+          this.address = result.address;
+        }
+        alert('Zbtprinter: connect success: ' + this.address);
+      }, function(error) {
+        alert('Zbtprinter: connect fail: ' + error);
+      }
+    );
+  }
 
-	getTotal() {
-		let total = 0.0;
-		for(var i=0; i<this.orderObj.selectedProducts.length;i++) {
-			total += this.orderObj.selectedProducts[i].product.price * this.orderObj.selectedProducts[i].quantity;
-		}
-		return total;
-	}
+  getTotal() {
+    let total = 0.0;
+    for (var i = 0; i < this.orderObj.selectedProducts.length; i++) {
+      total += this.orderObj.selectedProducts[i].product.price * this.orderObj.selectedProducts[i].quantity;
+    }
+    return total;
+  }
 
-	dismiss() {
-		this.viewCtrl.dismiss();
-	}
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
 
-	cancel(){
-		this.viewCtrl.dismiss();
-	}
+  cancel() {
+    this.viewCtrl.dismiss();
+  }
 
-// New function to print in zebra lang
-  printOrder () {
+  // New function to print in zebra lang
+  printOrder() {
+    alert("impresion");
     console.log("impresion");
-    
+
 
     cordova.plugins.zbtprinter.print(this.address, "^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ",
       function(success) {
-          alert("Zbtprinter print success: " + success);
+        alert("Zbtprinter print success: " + success);
       }, function(fail) {
-          alert("Zbtprinter print fail:" + fail);
-          //deferred.reject(fail);
+        alert("Zbtprinter print fail:" + fail);
+        //deferred.reject(fail);
       }
     );
 
   }
 
-
+  findOrder() {
+    cordova.plugins.zbtprinter.find(
+      function(result) {
+        if (typeof result == 'string') {
+          this.address = result;
+        } else {
+          this.address = result.address;
+        }
+        alert('Zbtprinter: connect success: ' + this.address);
+      }, function(error) {
+        alert('Zbtprinter: connect fail: ' + error);
+      }
+    );
+  }
 
 }
