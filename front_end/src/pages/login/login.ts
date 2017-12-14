2bcc5b5637b55d285a4761deba76af51bbd48537
Carlos Paz <phoenixsampras@gmail.com>
2017-12-14 06:37:07 -0400

    login info

--- front_end/src/pages/login/login.ts
+++ front_end/src/pages/login/login.ts
@@ -3,7 +3,7 @@ import { NavController, LoadingController, ToastController } from 'ionic-angular
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import 'rxjs/add/operator/toPromise';
 import { TabsNavigationPage } from '../tabs-navigation/tabs-navigation';
-import { Jsonp  } from '@angular/http';
+import { Jsonp } from '@angular/http';
 import { OrdersService } from '../orders/orders.service';
 
 @Component({
@@ -11,65 +11,65 @@ import { OrdersService } from '../orders/orders.service';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-	login: FormGroup;
-	main_page: { component: any };
-	loading: any;
+  login: FormGroup;
+  main_page: { component: any };
+  loading: any;
 
 
-	constructor(
-		public nav: NavController,
-		public loadingCtrl: LoadingController,
-		public toastCtrl: ToastController,
-		public ordersService: OrdersService,
-		public jsonp: Jsonp,
+  constructor(
+    public nav: NavController,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController,
+    public ordersService: OrdersService,
+    public jsonp: Jsonp,
 
-	) {
-		this.main_page = { component: TabsNavigationPage };
+  ) {
+    this.main_page = { component: TabsNavigationPage };
 
-		this.login = new FormGroup({
-			email: new FormControl('', Validators.required),
-			password: new FormControl('', Validators.required)
-		});
-	}
+    this.login = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required)
+    });
+  }
 
-	doLogin(values){
-		let loadingCtrl = this.loadingCtrl;
-		let toastCtrl = this.toastCtrl;
-		let loading = loadingCtrl.create();
-		loading.present();
-		let nav = this.nav;
-		let me = this;
-		let url = 'http://cloud.movilcrm.com/organica/back_end/rmXMLRPC_login.php?task=loginApp';
-		url += '&username=' + values.email;
-		url += '&password=' + values.password;
-		url += '&callback=JSONP_CALLBACK';
-		this.jsonp.request(url,{method:'Get'})
-		.toPromise()
-		.then(data => {
-			loading.dismiss();
-			console.log(data['_body']['login']);
-			if(data['_body']['login'] == 'False' ){
-				let toast = toastCtrl.create({
-					message: "Incorrecto, intente nuevamente.",
-					duration: 3000,
-					cssClass: 'toast-error',
-					position:'bottom',
-				});
-				toast.present();
-			} else {
-				me.ordersService.loginId = data['_body']['login'];
-				let toast = toastCtrl.create({
-					message: "Bienvenido!",
-					duration: 3000,
-					cssClass: 'toast-success',
-					position:'bottom',
-				});
-				toast.present();
-				nav.setRoot(me.main_page.component);
-			}
-			//console.log(data.login);
-		});
-		//
-	}
+  doLogin(values) {
+    let loadingCtrl = this.loadingCtrl;
+    let toastCtrl = this.toastCtrl;
+    let loading = loadingCtrl.create();
+    loading.present();
+    let nav = this.nav;
+    let me = this;
+    let url = 'http://cloud.movilcrm.com/organica/back_end/rmXMLRPC_login.php?task=loginApp';
+    url += '&username=' + values.email;
+    url += '&password=' + values.password;
+    url += '&callback=JSONP_CALLBACK';
+    this.jsonp.request(url, { method: 'Get' })
+      .toPromise()
+      .then(data => {
+        loading.dismiss();
+        console.log(JSON.stringify(data['_body']));
+        if (data['_body']['login'] == 'False') {
+          let toast = toastCtrl.create({
+            message: "Incorrecto, intente nuevamente.",
+            duration: 3000,
+            cssClass: 'toast-error',
+            position: 'bottom',
+          });
+          toast.present();
+        } else {
+          me.ordersService.loginId = data['_body']['login'];
+          let toast = toastCtrl.create({
+            message: "Bienvenido!",
+            duration: 3000,
+            cssClass: 'toast-success',
+            position: 'bottom',
+          });
+          toast.present();
+          nav.setRoot(me.main_page.component);
+        }
+        //console.log(data.login);
+      });
+    //
+  }
 
 }
