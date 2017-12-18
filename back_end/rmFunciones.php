be68a741f9bf49a18eeca6758df01a6c70d90711
Carlos Paz <phoenixsampras@gmail.com>
2017-12-18 12:17:18 -0400

    clientes xmlrpc

---
+++ back_end/rmFunciones.php
@@ -0,0 +1,11 @@
+<?php
+function utf8_converter($array)
+{
+  array_walk_recursive($array, function(&$item, $key){
+    if(!mb_detect_encoding($item, 'utf-8', true)){
+      $item = utf8_encode($item);
+    }
+  });
+  return $array;
+}
+?>
