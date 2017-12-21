41a647e7f07cceb2998e95c87c23e1f8684e18be
Carlos Paz <phoenixsampras@gmail.com>
2017-12-21 12:34:33 -0400

    print order numbering and fix

--- front_end/src/pages/print-order/print-order.ts
+++ front_end/src/pages/print-order/print-order.ts
@@ -37,12 +37,12 @@ export class PrintOrderPage {
     this.orderObj = this.navParams.get('order');
     let me = this;
     this.databaseService.getLoginData()
-    .then(
+      .then(
       response => {
         me.loginObj = response;
         console.log(me.loginObj);
       }
-    );
+      );
 
   }
 
@@ -101,12 +101,12 @@ export class PrintOrderPage {
     if (this.orderObj) {
       moment.lang('es-us');
       this.orderObj.confirmed = true;
-	    if(!this.orderObj.numberOrder || this.orderObj.numberOrder == 0) {
-		  let co = this.ordersService.confirmedOrders ? this.ordersService.confirmedOrders : 0;
-		  this.ordersService.confirmedOrders = co + 1;
-		  this.orderObj.numberOrder = this.ordersService.confirmedOrders;
-		}
-		this.ordersService.updateOrder(this.orderObj);
+      if (!this.orderObj.numberOrder || this.orderObj.numberOrder == 0) {
+        let co = this.ordersService.confirmedOrders ? this.ordersService.confirmedOrders : 0;
+        this.ordersService.confirmedOrders = co + 1;
+        this.orderObj.numberOrder = this.ordersService.confirmedOrders;
+      }
+      this.ordersService.updateOrder(this.orderObj);
 
       // alert("impresion");
       console.log("impresion");
@@ -127,116 +127,120 @@ export class PrintOrderPage {
       let phone = '';
       let vendedor = this.loginObj.rmDatosUsuario.name;
 
-      console.log("orderObj:" + JSON.stringify(this.orderObj));
-      // $window.orderObj = this.orderObj;
+      // Imprimir solamente pedidos con numeracion
+      let numero = this.orderObj.numberOrder;
+      if (numero > 0) {
+        console.log("orderObj:" + JSON.stringify(this.orderObj));
+        // $window.orderObj = this.orderObj;
 
-      zebra_receipt_header = `
-              ^XA
-              ^MNN
-              ^LL1100
-              ^ASN,50
-              ^FO0,30^FB580,3,0,C,0^FD` + empresa + `^FS
-              ^FO0,70^FB580,3,0,C,0^FD` + phone + `^FS
-              ^FO0,110^FB580,3,0,C,0^FDCochabamba - Bolivia^FS
-              ^CFQ
-              ^FO5,180^FD Fecha: ` + moment(this.orderObj.dateOrder).format('D MMMM YYYY, h:mm a') + `^FS
-              ^FO5,210^FD Vendedor: ` + vendedor + `^FS
-              ^FO5,240^FD Cliente: ` + this.orderObj.customerObj.name + `^FS
-              ^FO5,270^FD NIT: ` + this.orderObj.customerObj.nit + `^FS
-              ^FO5,300^FD Direccion: ` + this.orderObj.customerObj.street + `^FS
-              ^FO5,330^FD Telefono: ` + this.orderObj.customerObj.phone + `^FS
-              ^FO5,360^FD Celular: ` + this.orderObj.customerObj.mobile + `^FS
+        zebra_receipt_header = `
+        ^XA
+        ^MNN
+        ^LL1100
+        ^ASN,50
+        ^FO0,30^FB580,3,0,C,0^FD` + empresa + `^FS
+        ^FO0,70^FB580,3,0,C,0^FDPEDIDO NUMERO: ` + numero + `^FS
+        ^FO0,110^FB580,3,0,C,0^FDCochabamba - Bolivia^FS
+        ^CFQ
+        ^FO5,180^FD Fecha: ` + moment(this.orderObj.dateOrder).format('D MMMM YYYY, h:mm a') + `^FS
+        ^FO5,210^FD Vendedor: ` + vendedor + `^FS
+        ^FO5,240^FD Cliente: ` + this.orderObj.customerObj.name + `^FS
+        ^FO5,270^FD NIT: ` + this.orderObj.customerObj.nit + `^FS
+        ^FO5,300^FD Direccion: ` + this.orderObj.customerObj.street + `^FS
+        ^FO5,330^FD Telefono: ` + this.orderObj.customerObj.phone + `^FS
+        ^FO5,360^FD Celular: ` + this.orderObj.customerObj.mobile + `^FS
 
-              ^CFF,20
-              ^LRY
-              ^FO0,400^GB600,40,30^FS
-              ^FO0,410^FDPRODUCTO       CANT PRECIO    TOTAL^FS
-              ^CFQ
-              `;
+        ^CFF,20
+        ^LRY
+        ^FO0,400^GB600,40,30^FS
+        ^FO5,410^FDPRODUCTO       CANT PRECIO    TOTAL^FS
+        ^CFQ
+        `;
 
-      for (var i = 0; i < this.orderObj.selectedProducts.length; i++) {
-        // console.log("dasda" + JSON.stringify(this.orderObj.selectedProducts[i]['product']['product']));
-        // console.log("dasda" + JSON.stringify(this.orderObj.selectedProducts[i]));
-        let productCountLetters = this.orderObj.selectedProducts[i]['product']['product'].length;
-        let quantity = (this.orderObj.selectedProducts[i]['quantity']);
-        let price = (this.getProductPrice(this.orderObj.selectedProducts[i]));
-        let subtotal = (price * this.orderObj.selectedProducts[i]['quantity']);
-        // let subtotal = (price * this.orderObj.selectedProducts[i]['quantity']).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
-        // let price = this.orderObj.selectedProducts[i]['product']['price'];
-        // let quantity = this.orderObj.selectedProducts[i]['quantity'].toLocaleString('en-US');
-        // let price = this.orderObj.selectedProducts[i]['product']['price'].toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
-        console.log("productCountLetters:" + productCountLetters);
-        zebra_receipt_body += `
-              ^FO0,` + zebra_receipt_body_height + `
-              ^FB250,5,0,L,0
-              ^FD` + this.orderObj.selectedProducts[i]['product']['product'] + `^FS
+        for (var i = 0; i < this.orderObj.selectedProducts.length; i++) {
+          // console.log("dasda" + JSON.stringify(this.orderObj.selectedProducts[i]['product']['product']));
+          // console.log("dasda" + JSON.stringify(this.orderObj.selectedProducts[i]));
+          let productCountLetters = this.orderObj.selectedProducts[i]['product']['product'].length;
+          let quantity = (this.orderObj.selectedProducts[i]['quantity']);
+          let price = (this.getProductPrice(this.orderObj.selectedProducts[i]));
+          let subtotal = (price * this.orderObj.selectedProducts[i]['quantity']);
+          // let subtotal = (price * this.orderObj.selectedProducts[i]['quantity']).toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+          // let price = this.orderObj.selectedProducts[i]['product']['price'];
+          // let quantity = this.orderObj.selectedProducts[i]['quantity'].toLocaleString('en-US');
+          // let price = this.orderObj.selectedProducts[i]['product']['price'].toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+          console.log("productCountLetters:" + productCountLetters);
+          zebra_receipt_body += `
+          ^FO0,` + zebra_receipt_body_height + `
+          ^FB250,5,0,L,0
+          ^FD` + this.orderObj.selectedProducts[i]['product']['product'] + `^FS
 
-              ^FO250,` + zebra_receipt_body_height + `
-              ^FB60,5,0,R,0
-              ^FD` + quantity + `^FS
+          ^FO250,` + zebra_receipt_body_height + `
+          ^FB60,5,0,R,0
+          ^FD` + quantity + `^FS
 
-              ^FO320,` + zebra_receipt_body_height + `
-              ^FB100,5,0,R,0
-              ^FD` + price + `^FS
+          ^FO320,` + zebra_receipt_body_height + `
+          ^FB100,5,0,R,0
+          ^FD` + price + `^FS
 
-              ^FO410,` + zebra_receipt_body_height + `
-              ^FB150,5,0,R,0
-              ^FD` + subtotal + `^FS
-              `;
-        zebra_receipt_body_height = zebra_receipt_body_height + 30;
-        zebra_receipt_body_total_height += 30;
-        if (productCountLetters > 22) {
+          ^FO410,` + zebra_receipt_body_height + `
+          ^FB150,5,0,R,0
+          ^FD` + subtotal + `^FS
+          `;
+          zebra_receipt_body_height = zebra_receipt_body_height + 30;
           zebra_receipt_body_total_height += 30;
-          zebra_receipt_body_height += 30;
+          if (productCountLetters > 22) {
+            zebra_receipt_body_total_height += 30;
+            zebra_receipt_body_height += 30;
+          }
         }
+
+        zebra_receipt_total_height += zebra_receipt_body_total_height;
+
+        let gran_total = this.orderObj.total.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        zebra_receipt_total = `
+        ^FO5,` + (zebra_receipt_total_height - 5) + `^GB600,3,3^FS
+        ^FO0,` + zebra_receipt_total_height + `
+        ^FB250,5,0,L,0
+        ^FDGRAN TOTAL^FS
+
+        ^FO250,` + zebra_receipt_total_height + `
+        ^FB60,5,0,R,0
+        ^FD^FS
+
+        ^FO410,` + zebra_receipt_total_height + `
+        ^FB150,5,0,R,0
+        ^FD` + gran_total + `^FS
+        `;
+
+        zebra_receipt_footer_height += zebra_receipt_body_total_height;
+        zebra_receipt_footer = `
+        ^CFR,15
+        ^FO5,` + (zebra_receipt_footer_height - 15) + `^GB600,1,1^FS
+        ^FO5,` + zebra_receipt_footer_height + `^FB580,3,0,C,0^FDFirma Cliente^FS
+        ^FO5,` + (zebra_receipt_footer_height + 140) + `^GB600,1,1^FS
+        ^FO5,` + (zebra_receipt_footer_height + 150) + `^FB580,3,0,C,0^FDFirma Vendedor^FS
+        ^FO5,` + (zebra_receipt_footer_height + 200) + `^FB580,3,0,C,0^FDGracias por su Compra!!^FS
+        ^FO5,` + (zebra_receipt_footer_height + 230) + `^FB580,3,0,C,0^FDFelices Fiestas!!^FS
+        ^FO0,` + (zebra_receipt_footer_height + 260) + `^FB580,3,0,C,0^FD^FS
+        ^XZ
+        `;
+
+        zebra_receipt = zebra_receipt_header + zebra_receipt_body + zebra_receipt_total + zebra_receipt_footer;
+        // zebra_receipt = 'sd';
+        // console.log("zebra_receipt_header:" + zebra_receipt_header);
+        // console.log("zebra_receipt_body:" + zebra_receipt_body);
+        // console.log("zebra_receipt_total:" + zebra_receipt_total);
+        // console.log("zebra_receipt_footer:" + zebra_receipt_footer);
+        // console.log("zebra_receipt_body_height:" + zebra_receipt_body_height);
+        // console.log("zebra_receipt_body_total_height:" + zebra_receipt_body_total_height);
+        // console.log("zebra_receipt_total_height:" + zebra_receipt_total_height);
+        // console.log("zebra_receipt_footer_height:" + zebra_receipt_footer_height);
+        console.log("zebra_receipt:" + zebra_receipt);
+        //alert(this.ordersService.macAddress);
+        // cordova.plugins.zbtprinter.print(this.ordersService.macAddress, "^XA^FO20,20^A0N,25,25^FD " + zebra_receipt + " ^FS^XZ",
+        // this.print2Zebra(zebra_receipt);
+        this.print2Zebra(zebra_receipt);
       }
-
-      zebra_receipt_total_height += zebra_receipt_body_total_height;
-
-      let gran_total = this.orderObj.total.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-      zebra_receipt_total = `
-              ^FO5,` + (zebra_receipt_total_height - 5) + `^GB600,3,3^FS
-              ^FO0,` + zebra_receipt_total_height + `
-              ^FB250,5,0,L,0
-              ^FDGRAN TOTAL^FS
-
-              ^FO250,` + zebra_receipt_total_height + `
-              ^FB60,5,0,R,0
-              ^FD^FS
-
-              ^FO410,` + zebra_receipt_total_height + `
-              ^FB150,5,0,R,0
-              ^FD` + gran_total + `^FS
-              `;
-
-      zebra_receipt_footer_height += zebra_receipt_body_total_height;
-      zebra_receipt_footer = `
-              ^CFR,15
-              ^FO5,` + (zebra_receipt_footer_height - 15) + `^GB600,1,1^FS
-              ^FO5,` + zebra_receipt_footer_height + `^FB580,3,0,C,0^FDFirma Cliente^FS
-              ^FO5,` + (zebra_receipt_footer_height + 140) + `^GB600,1,1^FS
-              ^FO5,` + (zebra_receipt_footer_height + 150) + `^FB580,3,0,C,0^FDFirma Vendedor^FS
-              ^FO5,` + (zebra_receipt_footer_height + 200) + `^FB580,3,0,C,0^FDGracias por su Compra!!^FS
-              ^FO5,` + (zebra_receipt_footer_height + 230) + `^FB580,3,0,C,0^FDFelices Fiestas!!^FS
-              ^FO0,` + (zebra_receipt_footer_height + 260) + `^FB580,3,0,C,0^FD^FS
-              ^XZ
-              `;
-
-      zebra_receipt = zebra_receipt_header + zebra_receipt_body + zebra_receipt_total + zebra_receipt_footer;
-      // zebra_receipt = 'sd';
-      // console.log("zebra_receipt_header:" + zebra_receipt_header);
-      // console.log("zebra_receipt_body:" + zebra_receipt_body);
-      // console.log("zebra_receipt_total:" + zebra_receipt_total);
-      // console.log("zebra_receipt_footer:" + zebra_receipt_footer);
-      // console.log("zebra_receipt_body_height:" + zebra_receipt_body_height);
-      // console.log("zebra_receipt_body_total_height:" + zebra_receipt_body_total_height);
-      // console.log("zebra_receipt_total_height:" + zebra_receipt_total_height);
-      // console.log("zebra_receipt_footer_height:" + zebra_receipt_footer_height);
-      console.log("zebra_receipt:" + zebra_receipt);
-      //alert(this.ordersService.macAddress);
-      // cordova.plugins.zbtprinter.print(this.ordersService.macAddress, "^XA^FO20,20^A0N,25,25^FD " + zebra_receipt + " ^FS^XZ",
-      // this.print2Zebra(zebra_receipt);
-      this.print2Zebra(zebra_receipt);
     }
   }
 
@@ -262,7 +266,7 @@ export class PrintOrderPage {
           position: 'bottom',
         });
         toast.present();
-        me.navCtrl.pop();
+        // me.navCtrl.pop();
 
       }
     );
