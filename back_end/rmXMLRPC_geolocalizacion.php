ed303ebe94777fb5e7408930f00d1e06392e25e9
Carlos Paz <carlos.paz@movilcrm.com>
2018-02-14 15:24:16 -0400

    geolocation url fix

--- back_end/rmXMLRPC_geolocalizacion.php
+++ back_end/rmXMLRPC_geolocalizacion.php
@@ -74,62 +74,89 @@ function rmListaGeolocalizacion ($db) {
 // Lista de geocercas
 function rmListaGeolocalizacionGeocerca ($db) {
     try {
-        $sql_geocerca = "
-        SELECT
-        rm_geocerca.id as geocerca_id,
-        rm_geocerca.name as geocerca_name
-        FROM rm_geocerca;
-        ";
+      $sql_geocerca = "
+      SELECT
+      rm_geocerca.id as geocerca_id,
+      rm_geocerca.name as geocerca_name
+      FROM rm_geocerca;
+      ";
 
-        $sql_geocerca_users = "
-        SELECT
-        rm_geocerca.id as geocerca_id,
-        res_users.id as user_id,
-        res_users.login
-        FROM rm_geocerca
-        INNER JOIN users_geocerca ON users_geocerca.rm_geocerca_id=rm_geocerca.id
-        INNER JOIN res_users ON res_users.id=users_geocerca.res_users_id
-        ORDER by rm_geocerca.id;
-        ";
+      $sql_geocerca_users = "
+      SELECT
+      rm_geocerca.id as geocerca_id,
+      res_users.id as user_id,
+      res_users.login
+      FROM rm_geocerca
+      INNER JOIN users_geocerca ON users_geocerca.rm_geocerca_id=rm_geocerca.id
+      INNER JOIN res_users ON res_users.id=users_geocerca.res_users_id
+      ORDER by rm_geocerca.id;
+      ";
 
-        $sql_geocerca_locations = "
-        SELECT
-        rm_geocerca.id as geocerca_id,
-        rm_geocerca.name as geocerca_name
-        ,rm_coordenadas_geocerca.rm_longitude
-        ,rm_coordenadas_geocerca.rm_latitude
-        FROM rm_geocerca
-        INNER JOIN rm_coordenadas_geocerca ON rm_coordenadas_geocerca.geocerca_id=rm_geocerca.id
-        ORDER by geocerca_id;
-        ";
+      $sql_geocerca_locations = "
+      SELECT
+      rm_geocerca.id as geocerca_id,
+      rm_geocerca.name as geocerca_name
+      ,rm_coordenadas_geocerca.rm_longitude
+      ,rm_coordenadas_geocerca.rm_latitude
+      FROM rm_geocerca
+      INNER JOIN rm_coordenadas_geocerca ON rm_coordenadas_geocerca.geocerca_id=rm_geocerca.id
+      ORDER by geocerca_id;
+      ";
 
-        $query = pg_query($db, $sql_geocerca);
-        if(!$query){
-          echo "Error".pg_last_error($db);
-        exit;
+      $query = pg_query($db, $sql_geocerca);
+      if(!$query){
+        echo "Error".pg_last_error($db);
+      exit;
+      }
+      $resultado1 = pg_fetch_all($query);
+
+      $query2 = pg_query($db, $sql_geocerca_users);
+      if(!$query2){
+        echo "Error".pg_last_error($db);
+      exit;
+      }
+      $resultado2 = pg_fetch_all($query2);
+
+      $query3 = pg_query($db, $sql_geocerca_locations);
+      if(!$query3){
+        echo "Error".pg_last_error($db);
+      exit;
+      }
+      $resultado3 = pg_fetch_all($query3);
+
+      // print_r($resultado3);
+      // print_r($resultado2);
+      // print_r($resultado3);
+
+      for ($i = 0; $i < count($resultado1); $i++) {
+        $users = array();
+        for ($z = 0; $z < count($resultado2); $z++) {
+          if ($resultado1[$i]['geocerca_id'] == $resultado2[$z]['geocerca_id']) {
+            $users[] = array('id'=>$resultado2[$z]['user_id'] ,'login'=>$resultado2[$z]['login']);
+          }
         }
-        $resultado1 = pg_fetch_all($sql_geocerca);
 
-        $query2 = pg_query($db, $sql_geocerca_users);
-        if(!$query2){
-          echo "Error".pg_last_error($db);
-        exit;
+        $locations = array();
+        for ($y = 0; $y < count($resultado3); $y++) {
+          if ($resultado1[$i]['geocerca_id'] == $resultado3[$y]['geocerca_id']) {
+            $locations[] = array('rm_longitude'=>$resultado3[$y]['rm_longitude'] ,'rm_latitude'=>$resultado3[$y]['rm_latitude']);
+          }
         }
-        $resultado2 = pg_fetch_all($sql_geocerca_users);
 
-        $query3 = pg_query($db, $sql_geocerca_locations);
-        if(!$query3){
-          echo "Error".pg_last_error($db);
-        exit;
-        }
-        $resultado3 = pg_fetch_all($sql_geocerca_locations);
+        $geofences[] =
+          array(
+            'geofence'=>array(
+              'id'=>$resultado1[$i]['geocerca_id'],
+              'name'=>$resultado1[$i]['geocerca_name'],
+              'users'=>$users,
+              'locations'=>$locations
+            )
+          );
+      }
 
-        print_r($resultado1);
-        print_r($resultado2);
-        print_r($resultado3);
 
-        echo $_GET['callback'].'({"rmListaGeolocalizacionGeocerca333": ' . json_encode($resultado) . '})';
-        pg_close($db);
+      echo $_GET['callback'].'({"rmListaGeolocalizacionGeocerca333": ' . json_encode($geofences) . '})';
+      pg_close($db);
 
     } catch(PDOException $e) {
         echo $_GET['callback'].'({"error":{"text":'. pg_last_error($db) .'}})';
